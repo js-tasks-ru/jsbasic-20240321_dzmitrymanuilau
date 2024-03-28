4d126e4a336c72b3c40141d82b1770f0e6c4047c
DzmitryManuilau <dzmitrymanuilau@gmail.com>
2024-03-28 19:04:51 +0300

    Add JavaScript sum function

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,7 +1,6 @@
 function sum(m, n) {
   return m + n;
-  }
-  let result1 = sum(1, 1);
-  let result2 = sum(2, 5);
-  alert (result1);
-  alert (result2);
+}
+
+console.log(sum(1, 1));
+console.log(sum(2, 5));
