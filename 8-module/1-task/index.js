e05e8dc214755866da89c95d52464c3ddc22d65f
DzmitryManuilau <dzmitrymanuilau@gmail.com>
2024-04-24 23:13:18 +0300

    the icon is fixedly positioned and visible on the page

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,6 +39,46 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
-  }
+    if (!this.elem.offsetWidth && !this.elem.offsetHeight) {
+      return; // Иконка корзины скрыта
+    }
+  
+    if (window.innerWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        right: '',
+        zIndex: ''
+      });
+      return; // Не перемещаем иконку в "мобильном" режиме
+    }
+  
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    let initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+  
+    if (scrollTop > initialTopCoord - this.elem.offsetHeight) {
+      this.elem.style.position = 'fixed';
+      this.elem.style.top = '50px'; // Фиксируем иконку на 50px сверху
+      this.elem.style.zIndex = '1000'; // Повышаем z-index
+  
+      let container = document.querySelector('.container');
+      if (container) {
+        let containerRect = container.getBoundingClientRect();
+        let containerRightEdge = containerRect.right + 20; // 20px правее контейнера
+        let screenRightEdge = document.documentElement.clientWidth - 10; // 10px от правого края окна
+        let maxRightPosition = Math.min(containerRightEdge, screenRightEdge); // Находим меньшее значение
+  
+        this.elem.style.right = `${document.documentElement.clientWidth - maxRightPosition}px`;
+      } else {
+        this.elem.style.right = '10px'; // Если контейнер не найден, устанавливаем 10px от правого края
+      }
+    } else {
+      Object.assign(this.elem.style, {
+        position: 'absolute', // Возвращаем абсолютное позиционирование
+        top: '',
+        right: '',
+        zIndex: ''
+      });
+    }
+  }  
 }
